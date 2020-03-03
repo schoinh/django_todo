0212f220691e67808451bc00bd4e4d256214d90f
Na Hyung Choi <schoinh@gmail.com>
2020-03-03 15:23:36 -0800

    Use Django links

--- todo_list/views.py
+++ todo_list/views.py
@@ -4,4 +4,7 @@ def home(request):
   return render(request, 'home.html', {})
 
 def about(request):
-  return render(request, 'about.html', {})
+  # also can define a dictionary (usually named context) here and refer to it in render()
+  my_name = 'Na Hyung Choi'
+  number = 2 + 2
+  return render(request, 'about.html', {'name': my_name, 'age': number})
