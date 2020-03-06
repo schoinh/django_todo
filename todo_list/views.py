3e0673cecdd85bed214257f4fee722ccf90895d7
Na Hyung Choi <schoinh@gmail.com>
2020-03-06 13:44:44 -0800

    Add item add capability

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 def home(request):
-  all_items = List.objects.all
-  return render(request, 'home.html', {'all_items': all_items})
+  if request.method == 'POST':
+    form = ListForm(request.POST or None)
+
+    if form.is_valid():
+      form.save()
+      all_items = List.objects.all
+      messages.success(request, ('Item has been added to list!'))
+      return render(request, 'home.html', {'all_items': all_items})
+
+  else:
+    all_items = List.objects.all
+    return render(request, 'home.html', {'all_items': all_items})
 
 def about(request):
   # also can define a dictionary (usually named context) here and refer to it in render()
