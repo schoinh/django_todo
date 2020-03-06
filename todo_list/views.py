119b98ab8ba963de788cc751afd8e472e4201af8
Na Hyung Choi <schoinh@gmail.com>
2020-03-06 14:10:24 -0800

    Add edit feature

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def home(request):
   if request.method == 'POST':
@@ -22,3 +23,36 @@ def about(request):
   my_name = 'Na Hyung Choi'
   number = 2 + 2
   return render(request, 'about.html', {'name': my_name, 'age': number})
+
+def delete(request, list_id):
+  item = List.objects.get(pk=list_id)
+  item.delete()
+  messages.success(request, ('Item has been deleted.'))
+  return redirect('home')
+
+def cross_off(request, list_id):
+  item = List.objects.get(pk=list_id)
+  item.completed = True
+  item.save()
+  return redirect('home')
+
+def uncross(request, list_id):
+  item = List.objects.get(pk=list_id)
+  item.completed = False
+  item.save()
+  return redirect('home')
+
+def edit(request, list_id):
+  if request.method == 'POST':
+    item = List.objects.get(pk=list_id)
+
+    form = ListForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+      form.save()
+      messages.success(request, ('Item has been edited.'))
+      return redirect('home')
+
+  else:
+    item = List.objects.get(pk=list_id)
+    return render(request, 'edit.html', {'item': item})
